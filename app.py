db5b4882762e81bf3ef6cfc8e2e7429edfdc91e4
Cleber Silva <noct@mackbook.local>
2022-11-14 07:57:27 -0300

    vercel commit

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+from bin.consumer import *
+from bin.helper import *
 import threading
 from fastapi import FastAPI
 
+
+
 app = FastAPI()
 set_free()
+
+c = threading.Thread(target=consume)
+c.start()
 
 @app.get("/")
 def read_root():
